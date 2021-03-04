a9c96660bf20f4b4950b7c5c892a344c484eb16a
PillNandHSLU <nando.piller@stud.hslu.ch>
2021-03-04 19:42:46 +0100

    First git commit!!!

---
+++ 40_Exercises/02_HelloWorld/hello.c
@@ -0,0 +1,10 @@
+//Kommentar
+#include <stdio.h> //#include <stdio.h> --> inkludiere standard input/output
+
+int main()
+{
+	printf("Hello World\n"); // \n fügt neue Zeile am Ende ein
+
+	return 0;
+
+}
